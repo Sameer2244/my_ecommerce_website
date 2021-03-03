8552a59b65ffd484782d12fd7a84fa1ad03881c2
sameer <sk5276873@gmail.com>
2021-03-03 10:41:39 +0530

    init

---
+++ src/main/java/com/springboot/ecommerce/onlineshop/OnlineshopApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ecommerce.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopApplication.class, args);
+	}
+
+}
